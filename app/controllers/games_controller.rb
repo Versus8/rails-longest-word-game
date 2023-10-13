f4f78484537e1ca2810b8c67b8109e0def7d22b3
Veronique Tureau <veronique.tureau@gmail.com>
2023-10-12 21:12:29 -0400

    longest word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,15 @@
 class GamesController < ApplicationController
   def new
     letters = []
-    10.times { letters.push(('a'..'z').to_a.sample) }
+    10.times { letters.push(('A'..'Z').to_a.sample) }
     @letters = letters
   end
 
   def score
+    params[:word].split('').each do |letter|
+      unless params[:letters].include?(letter)
+        @result = "Sorry but #{params[:word]} can't be build out of the letters"
+      end
+    end
   end
 end
